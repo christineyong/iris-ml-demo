4491498aef418001fd11c57eae809303d473761d
sideqst <christine@dsaid.gov.sg>
2022-06-28 21:34:25 +0800

    Load Iris dataset from scikit-learn

---
+++ iris_ml/data.py
@@ -0,0 +1,4 @@
+from sklearn.datasets import load_iris
+
+def get_data():
+    return load_iris()
---
+++ iris_ml/tests/test_unit.py
@@ -0,0 +1,8 @@
+'''
+Script containing data-related functions, classes and methods.
+'''
+
+from ..data import get_data
+
+def test_get_data():
+    get_data()
